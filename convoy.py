a9cfcd5f9dcfeb880c3fd488bb4e929e4b4d0e68
Valentin <Kindnessbel@mail.ru>
2021-08-26 00:19:24 +0300

    3 Stage. "Finally to S3DB"
    Populate your SQLite3 database with the corrected data. Pars corrected csv file and insert to data

--- convoy.py
+++ convoy.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from os import path
 import re
+import sqlite3
+
 
 def xlsx_to_csv(name_file):
     name_two = name_file.replace('.xlsx', '.csv')
@@ -36,9 +38,7 @@ def corrected_csv(name_file):
         return value
 
     for col in colums_tuple:
-            data[col] = data[col].apply(lambda x: regex_counter(x))
-
-
+        data[col] = data[col].apply(lambda x: regex_counter(x))
 
     data.to_csv(name_two, index=None, header=True)  # maybe correct mode='a'
 
@@ -47,15 +47,51 @@ def corrected_csv(name_file):
     else:
         print(str(count) + ' cells were corrected in ' + name_two)
 
+    return name_two
 
 
+def create_insert_sql_date(name_file):
+    name_two = name_file.replace('[CHECKED].csv', '.s3db')
+    data = pd.read_csv(name_file)
+    df = pd.DataFrame(data, dtype='str')
+    count = 0
+
+    conn = sqlite3.connect(name_two)
+    cursor = conn.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS convoy 
+(vehicle_id INT PRIMARY KEY,
+engine_capacity INT NOT NULL,
+fuel_consumption INT NOT NULL,
+maximum_load INT NOT NULL)""")
+
+    for row in df.itertuples():
+        cursor.execute('''
+                INSERT INTO convoy (vehicle_id, engine_capacity, fuel_consumption, maximum_load)
+                VALUES (?,?,?,?)
+                ''',
+                       [row.vehicle_id,
+                       row.engine_capacity,
+                       row.fuel_consumption,
+                       row.maximum_load]
+                       )
+        count += 1
+    conn.commit()
+
+    if count == 1:
+        print(str(count) + ' record was inserted into ' + name_two)
+    else:
+        print(str(count) + ' records were inserted into ' + name_two)
 
 def pick_option(option):
-    if '.csv' in option:
-        corrected_csv(option)
+    if '[CHECKED].csv' in option:
+        create_insert_sql_date(option)
+    elif '.csv' in option:
+        checked_file = corrected_csv(option)
+        create_insert_sql_date(checked_file)
     elif '.xlsx' in option:
         csv_file = xlsx_to_csv(option)
-        corrected_csv(csv_file)
+        checked_file = corrected_csv(csv_file)
+        create_insert_sql_date(checked_file)
 
 
 print('Input file name')
